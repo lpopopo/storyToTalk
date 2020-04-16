a9e19ba1b61b629312ea1cfeb5a774b60593d5ed
lpopopo <2403164743@qq.com>
2020-04-16 11:06:02 +0800

    token verify

--- api/token/tokenParser.js
+++ api/token/tokenParser.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 const pubkey  = '-----BEGIN public keycenter-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1WXAZZkfDo4H5AfnF82r\nnPZNEhBaDQKI6IT+CBI4uUSBc+iq9OeLuWy/9tqIwn7WC1BVhFQQNbbI44jTReCS\nh4/RXd5AOI/fBzLVr8qlEUSMUSX1JzhBfrfVCV/0tMMy1Bh6yn6Cb2LFgfBglUsE\ntuTZlCnmE2+V45paoGH2tO3mIqkopBAz5kIn+TJ7dhpxivIfRAx2HyVq4JJy4CQD\nJy/pZ6fYamnVl8p1IiONmU1hS8MDZysoxk5e6mv0OFVaXd8naoN9dlD7OmW86N+Y\n8k1qIVcBPIkAQp2N+APhIueolC4kgF8Zjlkbw9cJFFYA8f5D1DU5acv/wQt58Ypi\njwIDAQAB\n-----END public keycenter-----\n'
 
 //token验证
-const codeToHash = (code , signature) =>{
+const codeToVerify = (code , signature) =>{
     //首先对playload进行base64解码
     code = new Buffer(getBase64UrlUnescape(code), 'base64')
     // signature = new Buffer(getBase64UrlUnescape(signature), 'base64')
@@ -34,9 +34,9 @@ const getBase64UrlUnescape = str => {
     // return {
     //   payload: decodeBase64Url(segments[0]),
     // };
-    codeToHash(segments[0] , segments[1])
+    codeToVerify(segments[0] , segments[1])
   };
 
   const str = `eyJjbGFzcyI6IjAyMTExODA0IiwiY29sbGVnZSI6IuWFieeUteW3peeoi+WtpumZoi/ph43luoblm73pmYXljYrlr7zkvZPlrabpmaIgIiwiZXhwIjoiMTAyMzA0MTcxODUiLCJoZWFkSW1nVXJsIjoiaHR0cDovL3RoaXJkd3gucWxvZ28uY24vbW1vcGVuL3ZpXzMyL0FuVU00VW1nRWJ1cnVCVXhxMW5ObVdDbUw2a0FNUFJkbEZYYWNsV2xNS3NPeHQzdzc1c0pOQWJRR2ZlaDRNMWdoWTdiZDl4aWI3bnQ1SHZpYXFSVU1PaHcvMTMyIiwiaWF0IjoiMTU3NTcwMDMwNSIsIm1ham9yIjoiIiwibmlja25hbWUiOiLlpKnkurrkuqbkuZ3oobAiLCJyZWFsTmFtZSI6IumZiOWFiOWLpCAgICAgICAgICIsInJlZElkIjoiZjVjNDA3YTA3YTJmMGE4NzJiZWIzNDkzMWY2MzRjY2Q0NGRlYmU0NCIsInN0dU51bSI6IjIwMTgyMTA2NTMiLCJzdWIiOiJ4YnMifQ==.xQLRI2J/HqLuAb0yQ+5KsuU2yG0tjz3/4zentmXjLCdTtjcTuPFAk0f7JUcpWXUg94WKlOgs0OouIkZFviLIpf/o6cJlszAT8btXZhmjXh4LR7pSf27DJxv5XblObA8p1bdVFYp/EmBKKnphPAAvcEVU1Pr8IFZ8kTZvEKSXp1a4IKJR4HxQGeDIryms2ydGM2CqTDR4LluXD/0H8y2O0G+GcQaexwzczZbGB1sjeYR0DN2tbp3KQT8Qxb516otWgeIg5IWOxRVD9HKJtdIVrxR1OORKus0wEjx2D/Dl0IEqsiHW1rDfQS8r4q2s3/vaIs9QObs3iKYU+IDMmwu7Sg==`
 
-urldecode(str)
+ urldecode(str)
